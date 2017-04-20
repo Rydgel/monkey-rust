748e14479ce399e4c7752acda115fa757c055e66
Jérôme Mahuet <jerome.mahuet@gmail.com>
2017-04-20 14:24:54 +0200

    tests

---
+++ tests/monkey_test.rs
@@ -0,0 +1,38 @@
+extern crate monkey_lib;
+
+use std::fs::File;
+use std::io::prelude::*;
+use monkey_lib::lexer::*;
+use monkey_lib::lexer::token::*;
+use monkey_lib::parser::*;
+use monkey_lib::evaluator::object::*;
+use monkey_lib::evaluator::*;
+
+fn read_file(file_path: String) -> Result<String, ::std::io::Error> {
+    let mut file = File::open(file_path)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    Ok(contents)
+}
+
+#[test]
+fn test_example_hash() {
+    let code_string = read_file("examples/map-reduce.mk".to_owned()).ok().unwrap();
+    let mut evaluator = Evaluator::new();
+    let lex_tokens = Lexer::lex_tokens(code_string.as_bytes()).to_result().unwrap();
+    let tokens = Tokens::new(&lex_tokens);
+    let program = Parser::parse_tokens(tokens).to_result().unwrap();
+    let eval = evaluator.eval_program(program);
+    assert_eq!(eval, Object::Null);
+}
+
+#[test]
+fn test_reduce() {
+    let code_string = read_file("examples/hash.mk".to_owned()).ok().unwrap();
+    let mut evaluator = Evaluator::new();
+    let lex_tokens = Lexer::lex_tokens(code_string.as_bytes()).to_result().unwrap();
+    let tokens = Tokens::new(&lex_tokens);
+    let program = Parser::parse_tokens(tokens).to_result().unwrap();
+    let eval = evaluator.eval_program(program);
+    assert_eq!(eval, Object::Null);
+}
