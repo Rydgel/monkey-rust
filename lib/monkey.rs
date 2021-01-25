0a79d081932d2f8e38480bac8842f621313eb2b7
Jérôme Mahuet <jerome.mahuet@gmail.com>
2021-01-25 19:06:57 +0100

    Fixing Rustyline

--- lib/monkey.rs
+++ lib/monkey.rs
@@ -1,5 +1,3 @@
-#[macro_use]
-
 extern crate nom;
 
 pub mod lexer;
--- repl/main.rs
+++ repl/main.rs
@@ -1,32 +1,110 @@
 extern crate monkey_lib;
-extern crate rustyline;
 extern crate nom;
+extern crate rustyline;
+extern crate rustyline_derive;
 
-use rustyline::completion::FilenameCompleter;
-use rustyline::error::ReadlineError;
-use rustyline::{Config, CompletionType, Editor};
-use monkey_lib::lexer::*;
-use monkey_lib::lexer::token::*;
-use monkey_lib::parser::*;
 use monkey_lib::evaluator::*;
+use monkey_lib::lexer::token::*;
+use monkey_lib::lexer::*;
+use monkey_lib::parser::*;
 use nom::*;
+use rustyline::completion::{Completer, FilenameCompleter, Pair};
+use rustyline::config::OutputStreamType;
+use rustyline::error::ReadlineError;
+use rustyline::highlight::{Highlighter, MatchingBracketHighlighter};
+use rustyline::hint::{Hinter, HistoryHinter};
+use rustyline::validate::{self, MatchingBracketValidator, Validator};
+use rustyline::{Cmd, CompletionType, Config, Context, EditMode, Editor, KeyEvent};
+use rustyline_derive::Helper;
+use std::borrow::Cow::{self, Borrowed, Owned};
 
-#[cfg(unix)]
-static PROMPT: &'static str = "\x1b[1;32mmonkey >>\x1b[0m ";
+#[derive(Helper)]
+struct MyHelper {
+    completer: FilenameCompleter,
+    highlighter: MatchingBracketHighlighter,
+    validator: MatchingBracketValidator,
+    hinter: HistoryHinter,
+    colored_prompt: String,
+}
 
-#[cfg(windows)]
-static PROMPT: &'static str = "monkey >> ";
+impl Completer for MyHelper {
+    type Candidate = Pair;
 
+    fn complete(
+        &self,
+        line: &str,
+        pos: usize,
+        ctx: &Context<'_>,
+    ) -> Result<(usize, Vec<Pair>), ReadlineError> {
+        self.completer.complete(line, pos, ctx)
+    }
+}
 
-fn main() {
+impl Hinter for MyHelper {
+    type Hint = String;
+
+    fn hint(&self, line: &str, pos: usize, ctx: &Context<'_>) -> Option<String> {
+        self.hinter.hint(line, pos, ctx)
+    }
+}
+
+impl Highlighter for MyHelper {
+    fn highlight_prompt<'b, 's: 'b, 'p: 'b>(
+        &'s self,
+        prompt: &'p str,
+        default: bool,
+    ) -> Cow<'b, str> {
+        if default {
+            Borrowed(&self.colored_prompt)
+        } else {
+            Borrowed(prompt)
+        }
+    }
+
+    fn highlight_hint<'h>(&self, hint: &'h str) -> Cow<'h, str> {
+        Owned("\x1b[1m".to_owned() + hint + "\x1b[m")
+    }
+
+    fn highlight<'l>(&self, line: &'l str, pos: usize) -> Cow<'l, str> {
+        self.highlighter.highlight(line, pos)
+    }
+
+    fn highlight_char(&self, line: &str, pos: usize) -> bool {
+        self.highlighter.highlight_char(line, pos)
+    }
+}
+
+impl Validator for MyHelper {
+    fn validate(
+        &self,
+        ctx: &mut validate::ValidationContext,
+    ) -> rustyline::Result<validate::ValidationResult> {
+        self.validator.validate(ctx)
+    }
+
+    fn validate_while_typing(&self) -> bool {
+        self.validator.validate_while_typing()
+    }
+}
+
+fn main() -> rustyline::Result<()> {
     let config = Config::builder()
         .history_ignore_space(true)
         .completion_type(CompletionType::List)
+        .edit_mode(EditMode::Emacs)
+        .output_stream(OutputStreamType::Stdout)
         .build();
-    let c = FilenameCompleter::new();
+    let h = MyHelper {
+        completer: FilenameCompleter::new(),
+        highlighter: MatchingBracketHighlighter::new(),
+        hinter: HistoryHinter {},
+        colored_prompt: "".to_owned(),
+        validator: MatchingBracketValidator::new(),
+    };
     let mut rl = Editor::with_config(config);
-    rl.set_completer(Some(c));
-
+    rl.set_helper(Some(h));
+    rl.bind_sequence(KeyEvent::alt('N'), Cmd::HistorySearchForward);
+    rl.bind_sequence(KeyEvent::alt('P'), Cmd::HistorySearchBackward);
     if rl.load_history("history.txt").is_err() {
         println!("No previous history.");
     }
@@ -37,12 +115,15 @@ fn main() {
     println!();
 
     let mut evaluator = Evaluator::new();
+    let mut count = 1;
 
     loop {
-        let readline = rl.readline(PROMPT);
+        let p = format!("{}> ", count);
+        rl.helper_mut().expect("No helper").colored_prompt = format!("\x1b[1;32m{}\x1b[0m", p);
+        let readline = rl.readline(&p);
         match readline {
             Ok(line) => {
-                rl.add_history_entry(line.as_ref());
+                rl.add_history_entry(line.as_str());
                 let lex_tokens = Lexer::lex_tokens(line.as_bytes());
                 match lex_tokens {
                     Ok((_, r)) => {
@@ -76,7 +157,8 @@ fn main() {
                 break;
             }
         }
+        count += 1;
     }
 
-    rl.save_history("history.txt").unwrap();
+    rl.append_history("history.txt")
 }
